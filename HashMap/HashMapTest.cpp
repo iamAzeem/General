acb22a39b5ba5e26d587a4f2c4d0e3f0311ad29c
Azeem Sajid <azeem.sajid@fireeye.com>
2016-06-30 09:49:43 +0500

    Added hashmap test skeleton code wtih makefile.

---
+++ HashMap/HashMapTest.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "hashmap.hpp"
+
+using std::cout;
+using std::endl;
+
+int main( void )
+{
+    return 0;
+}
+
---
+++ HashMap/hashmap.hpp
@@ -0,0 +1,8 @@
+#ifndef HASHMAP_HPP_
+#define HASHMAP_HPP_
+
+
+
+
+
+#endif /* HASHMAP_HPP_ */
