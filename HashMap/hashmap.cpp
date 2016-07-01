06532d463fa2201edde3245845e106f5d0c75f8a
Azeem Sajid <azeem.sajid@fireeye.com>
2016-07-01 14:00:35 +0500

    HashMap skeleton code.

--- HashMap/hashmap.cpp
+++ HashMap/hashmap.cpp
@@ -1,3 +1,85 @@
+#include <iostream>
+#include <cstdlib>
 #include "hashmap.hpp"
 
 
+namespace HashMapTest {
+
+template < typename K, typename V >
+class Entry
+{
+public:
+    Entry( const K& key, const V& value ) : _key( key ), _value( value ), _next( nullptr )
+    {
+    }
+
+    void setKey  ( const K& key )      { _key = key;     }
+    void setValue( const V& value )    { _value = value; }
+    void setNext ( const Entry* next ) { _next = next;   }
+
+    const K getKey  ( void ) const { return _key;   }
+    const V getValue( void ) const { return _value; }
+    Entry*  getNext ( void ) const { return _next;  }
+
+private:
+    K       _key;
+    V       _value;
+
+    Entry*  _next;
+};
+
+template < typename K, typename V, typename F >
+TSHashMap<K, V, F>::TSHashMap( const unsigned int size ) : _hashTable( nullptr ), _size( size )
+{
+    /* Validate positive size; use default size otherwise */
+    if ( size <= 0 )
+    {
+        _size = Default::SIZE;
+    }
+
+    /* Initialize hash table / buckets */
+    _hashTable = new Entry<K, V>*[ _size ];
+    if ( _hashTable == nullptr )
+    {
+        std::cerr << "Could not allocate memory for HashMap! Exiting..." << std::endl;
+        std::exit( EXIT_FAILURE );
+    }
+}
+
+template < typename K, typename V, typename F >
+TSHashMap<K, V, F>::~TSHashMap()
+{
+
+}
+
+template < typename K, typename V, typename F >
+void TSHashMap<K, V, F>::add ( const K& key, const V& value )
+{
+
+}
+
+template < typename K, typename V, typename F >
+void TSHashMap<K, V, F>::del ( const K& key )
+{
+
+}
+
+template < typename K, typename V, typename F >
+V TSHashMap<K, V, F>::find ( const K& key )
+{
+
+}
+
+template < typename K, typename V, typename F >
+const unsigned int TSHashMap<K, V, F>::size( void ) const
+{
+    return _size;
+}
+
+template < typename K, typename V, typename F >
+void TSHashMap<K, V, F>::resize( void )
+{
+
+}
+
+} // HashMapTest
--- HashMap/hashmap.hpp
+++ HashMap/hashmap.hpp
@@ -2,7 +2,38 @@
 #define HASHMAP_HPP_
 
 
+namespace HashMapTest {
 
+template < typename K, typename V >
+class Entry;
+
+template < typename K, typename V, typename F >
+class TSHashMap
+{
+public:
+    enum class Default : unsigned int { SIZE = 10 };
+
+    TSHashMap( const unsigned int size );
+
+    ~TSHashMap();
+
+    void add ( const K& key, const V& value );
+    void del ( const K& key );
+    V    find( const K& key );
+
+    const unsigned int size( void ) const;
+
+    void resize( void );
+
+private:
+    Entry<K, V>**   _hashTable;
+    F               _hashFunction;
+    unsigned int    _size;
+
+
+};
+
+} // HashMapTest
 
 
 #endif /* HASHMAP_HPP_ */
